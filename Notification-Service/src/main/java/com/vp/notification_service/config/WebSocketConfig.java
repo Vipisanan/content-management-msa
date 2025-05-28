64f28a6c9182bccc5a22119792b162431d8c5859
vipiBitbucket <vipivipisanan@gmail.com>
2025-05-29 00:55:57 +0530

    allow ws origin

--- Notification-Service/src/main/java/com/vp/notification_service/config/WebSocketConfig.java
+++ Notification-Service/src/main/java/com/vp/notification_service/config/WebSocketConfig.java
@@ -23,8 +23,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 //        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
-        registry.addEndpoint("api/ws")
-                .setAllowedOrigins(allowedOrigin)
+        registry.addEndpoint("/api/ws")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 }
